de6105bfba9edf46fe04e3d334c316779add2016
byuk <jsb100800@naver.com>
2021-05-25 02:32:35 +0900

    [Add] Connect Fragment

--- Android/app/src/main/java/com/example/classplus/MainActivity.java
+++ Android/app/src/main/java/com/example/classplus/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.classplus;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // FrameLayout에 각 메뉴의 Fragment를 바꿔 줌
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentTransaction transaction;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setStatusBar();
+        startActivity();
+    }
+
+    public void startActivity() {
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        // 첫 화면 지정
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_container,new MyPageFragment()).commitAllowingStateLoss();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavItemSelectedListener());
+    }
+
+    class BottomNavItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+
+                case R.id.bottom_navigation_mypage:
+                    replaceFragment(new MyPageFragment());
+                    break;
+
+                case R.id.bottom_navigation_chatting:
+                    replaceFragment(new ChattingFragment());
+                    break;
+
+                case R.id.bottom_navigation_total_chatting:
+                    replaceFragment(new TotalChattingFragment());
+                    break;
+
+                case R.id.bottom_navigation_setting:
+                    replaceFragment(new settingFragment());
+                    break;
+            }
+            return true;
+        }
+
+    }
+
+    //fragment 전환하는 메소드
+    public void replaceFragment(Fragment fragment) {
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_container, fragment).commit();
+    }
+
+    // 상태바 색 바꾸기
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setStatusBar() {
+        View view = getWindow().getDecorView();
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(Color.parseColor("#FFFFFF"));//색 지정
     }
 }
