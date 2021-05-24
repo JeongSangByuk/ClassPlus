faa50167319c0c8e79742f9ac12fd60c8979a727
qazyj <qazyj@naver.com>
2021-05-25 00:40:41 +0900

    server init commit

---
+++ Server/classplus/src/main/java/com/classplus/classplus/ClassplusApplication.java
@@ -0,0 +1,13 @@
+package com.classplus.classplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassplusApplication.class, args);
+	}
+
+}
---
+++ Server/classplus/src/test/java/com/classplus/classplus/ClassplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classplus.classplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
