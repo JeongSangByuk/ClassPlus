47ffd7c954f51697a3bf89a7d655b485032d1f7f
sangbyuk <jsb100800@naver.com>
2021-05-28 16:15:02 +0900

    [Add] login Page

---
+++ Android/app/src/main/java/com/example/classplus/LogInActivity.java
@@ -0,0 +1,47 @@
+package com.example.classplus;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogInActivity extends AppCompatActivity {
+
+
+    private TextView logInButton;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setStatusBar();
+        setContentView(R.layout.activity_login);
+
+        logInButton = findViewById(R.id.btn_login_activity);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent); //다음화면으로 넘어감
+                finish();
+            }
+        });
+    }
+
+    // 상태바 색 바꾸기
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setStatusBar() {
+        View view = getWindow().getDecorView();
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(Color.parseColor("#00000000"));//색 지정
+    }
+}
