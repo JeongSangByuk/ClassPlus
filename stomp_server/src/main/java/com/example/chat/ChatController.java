2bb2080d14f2f2e186e9dbdb7a04a6993aeafa17
sawon choi <sawon49@naver.com>
2021-06-01 00:02:20 +0900

    [Add] stomp test

---
+++ stomp_server/src/main/java/com/example/chat/ChatController.java
@@ -0,0 +1,18 @@
+package com.example.chat;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController
+{
+    @MessageMapping("/chat/{topic}")
+    @SendTo("/topic/messages")
+    public OutputMessage send(@DestinationVariable("topic") String topic,
+                              Message message) throws Exception
+    {
+        return new OutputMessage(message.getFrom(), message.getText(), topic);
+    }
+}
