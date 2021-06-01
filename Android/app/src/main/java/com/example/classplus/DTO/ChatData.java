deaa0d9182686d31bb3c99e01a20acdc6fcf8ace
sawon choi <sawon49@naver.com>
2021-06-01 11:52:05 +0900

    메시지 타입 지정

--- Android/app/src/main/java/com/example/classplus/DTO/ChatData.java
+++ Android/app/src/main/java/com/example/classplus/DTO/ChatData.java
@@ -3,11 +3,19 @@ package com.example.classplus.DTO;
 import com.example.classplus.R;
 
 public class ChatData {
+
+    public enum MessageType {
+        ENTER, TALK, EXIT, WORK_STACK
+    }
+
     private String userName;
     private String message;
     private String time;
     private int userImg;
     private boolean isMe;
+
+
+    private MessageType type;
 
     public ChatData(){}
 
@@ -25,7 +33,19 @@ public class ChatData {
         this.time = time;
         this.userImg = userImg;
         this.isMe = isMe;
+        type = MessageType.TALK;
     }
+
+
+    public ChatData(String userName, String message, String time, int userImg, boolean isMe, MessageType type) {
+        this.userName = userName;
+        this.message = message;
+        this.time = time;
+        this.userImg = userImg;
+        this.isMe = isMe;
+        this.type = type;
+    }
+
 
     public boolean isMe() {
         return isMe;
@@ -65,5 +85,14 @@ public class ChatData {
 
     public void setUserImg(int userImg) {
         this.userImg = userImg;
+    }
+
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
     }
 }
