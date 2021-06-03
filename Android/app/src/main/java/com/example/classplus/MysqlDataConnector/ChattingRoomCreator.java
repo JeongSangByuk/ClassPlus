190a58e31d5efc324c81db1e094c554dcb015bbd
qazyj <qazyj@naver.com>
2021-06-03 23:52:40 +0900

    [UPDATE/PHP] 수정

--- Android/app/src/main/java/com/example/classplus/MysqlDataConnector/ChattingRoomCreator.java
+++ Android/app/src/main/java/com/example/classplus/MysqlDataConnector/ChattingRoomCreator.java
@@ -2,7 +2,6 @@ package com.example.classplus.MysqlDataConnector;
 
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
-import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -10,7 +9,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class ChattingNameSender extends AsyncTask<String, Void, String> {
+public class ChattingRoomCreator extends AsyncTask<String, Void, String> {
     ProgressDialog progressDialog;
 
     @Override
